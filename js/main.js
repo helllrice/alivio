6c4cc5f35e87f33dab8d8389f3699f4f5624e88f
vasiliy <wizardcatt@gmail.com>
2022-06-11 10:00:06 +0300

    работа с видео и бургер-меню

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
+//mobile nav//
+
 const btn = document.querySelector('.mobile-nav-button');
 const icon = document.querySelector('.mobile-nav-button__icon');
+const mobileNav = document.querySelector('.mobile-nav');
 
 btn.addEventListener('click', () => {
+    mobileNav.classList.toggle('active');
     icon.classList.toggle('active');
+    document.body.classList.toggle('no-scroll')
 })
+
+
+// video
+
+const videoBtn = document.querySelector('#story-video-btn');
+const videoFile = document.querySelector('#video-story');
+
+videoBtn.addEventListener('click', () => {
+
+    if (videoFile.paused) {
+        videoFile.play()
+    } else {
+        videoFile.pause()
+    }
+
+
+})
+
+
 
